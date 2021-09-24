2683f148f856596830322a1ce3cbbe81a75a8287
AdrianTejada <atejada@my.bcit.ca>
2021-09-23 22:07:23 -0700

    got ui kitten to work

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Camera } from 'expo-camera';
-import TextTag from './comps/text'
+import { Text, ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 
 export default function App() {
@@ -11,52 +12,17 @@ export default function App() {
   },[])
 
   return (
-    <View style={styles.container}>
-      <View style={{borderRadius:50,paddingBottom: 18}}>
-        <Camera style={{width:60, height:60,}} type={Camera.Constants.Type.front}/>
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <View style={styles.container}>
+        <View style={{borderRadius:50,paddingBottom: 18}}>
+          <Camera style={{width:60, height:60,}} type={Camera.Constants.Type.front}/>
+        </View>
+
+        <Text>yes</Text>
+
+        <StatusBar style="auto" />
       </View>
-
-      <Text style={boldtext.container}>Adrian</Text>
-      <Text style={smalltext.container}>@adriantejada</Text>
-
-        <View style={followcount.container}>
-          <View style={followcount.container}>
-            <Text style={boldtext.container}>20</Text>
-            <Text style={smalltext.container}>Following</Text>
-          </View>
-
-          <View style={followcount.container}>
-            <Text style={boldtext.container}>20</Text>
-            <Text style={smalltext.container}>Followers</Text>
-          </View>
-        </View>
-
-        <View>
-          <Text style={normaltext.container}>Profile</Text>
-          <Text style={normaltext.container}>Lists</Text>
-          <Text style={normaltext.container}>Topics</Text>
-          <Text style={normaltext.container}>Bookmarks</Text>
-          <Text style={normaltext.container}>Moments</Text>
-          <Text style={normaltext.container}>Purchase</Text>
-          <Text style={normaltext.container}>Monetization</Text>     
-          <Text style={normaltext.container}>Twitter Blue</Text>     
-        </View>
-
-        <View style={divider.container}></View>
-
-        <Text style={normaltext.container}>Settings and privacy</Text>
-        <Text style={normaltext.container}>Help Center</Text> 
-
-        <TextTag/>
-
-
-
-
-      
-
-
-      <StatusBar style="auto" />
-    </View>
+    </ApplicationProvider>
   );
 }
 
@@ -72,55 +38,5 @@ const styles = StyleSheet.create({
   },
 });
 
-const smalltext = StyleSheet.create({
-  container: {
-    color: '#454545',
-    marginLeft: 0,
-    fontSize: 17,
-    marginRight: 5
-  },
-});
-
-const boldtext = StyleSheet.create({
-  container: {
-    color: 'white',
-    marginLeft: 0,
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginRight: 3
-  },
-});
-
-const normaltext = StyleSheet.create({
-  container: {
-    color: 'white',
-    fontSize: 20,
-    paddingBottom: 15,
-    paddingTop: 15
-  },
-});
-
-const followcount = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    paddingTop: 5,
-    paddingBottom: 5,
-  }
-});
-
-const content = StyleSheet.create({
-  container: {
-    flexDirection: 'column'
-  }
-});
-
-const divider = StyleSheet.create({
-  container: {
-    backgroundColor: "#454545",
-    width: "130%",
-    height: .3,
-    left: -30
-  },
-});
 
 
